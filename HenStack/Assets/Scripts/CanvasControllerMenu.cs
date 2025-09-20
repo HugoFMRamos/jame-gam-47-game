731289063a86e61570d30d8f10eed5bbfafc4b2f
HugoFMRamos <106814889+HugoFMRamos@users.noreply.github.com>
2025-09-20 13:17:45 +0100

    MainMenu

--- HenStack/Assets/Scripts/CanvasControllerMenu.cs
+++ HenStack/Assets/Scripts/CanvasControllerMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 public class CanvasControllerMenu : MonoBehaviour
 {
     public static CanvasControllerMenu Instance { get; private set; }
+    public GameObject menuScreen;
+    public GameObject h2pscreen;
+    private bool onH2PScreen = false;
 
     void Awake()
     {
@@ -14,10 +17,25 @@ public class CanvasControllerMenu : MonoBehaviour
         }
         Instance = this;
 
+        menuScreen.SetActive(true);
+        h2pscreen.SetActive(false);
+
     }
 
     public void Play()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void HowToPlay()
+    {
+        onH2PScreen = !onH2PScreen;
+        menuScreen.SetActive(!onH2PScreen);
+        h2pscreen.SetActive(onH2PScreen);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
